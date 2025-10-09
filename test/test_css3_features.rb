65288c62567b90179d3eb33711f632d327dcef66
James Cook <jamescook@james-m1-personal.lan>
2025-10-09 12:31:08 -0500

    Add CSS3 attribute selector operators (^=, $=, *=)
    Implements CSS3 substring matching attribute selectors:
    - ^= (starts with): [href^="http"]
    - $= (ends with): [href$=".pdf"]
    - *= (contains): [class*="button"]
    
    Added test coverage in test/test_css3_features.rb for all three operators.

---
+++ test/test_css3_features.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "cataract"
+
+# Tests for CSS3 features
+class TestCSS3Features < Minitest::Test
+  def setup
+    @parser = Cataract::Parser.new
+  end
+
+  # ============================================================================
+  # CSS3 Attribute Selectors
+  # ============================================================================
+
+  def test_attribute_starts_with
+    # ^= matches if attribute value starts with specified string
+    css = '[href^="http"] { color: blue }'
+    @parser.parse(css)
+
+    assert_includes @parser.selectors, '[href^="http"]'
+  end
+
+  def test_attribute_ends_with
+    # $= matches if attribute value ends with specified string
+    css = '[href$=".pdf"] { text-decoration: none }'
+    @parser.parse(css)
+
+    assert_includes @parser.selectors, '[href$=".pdf"]'
+  end
+
+  def test_attribute_contains
+    # *= matches if attribute value contains specified substring
+    css = '[class*="button"] { cursor: pointer }'
+    @parser.parse(css)
+
+    assert_includes @parser.selectors, '[class*="button"]'
+  end
+end
