1c1f56fa6fa7aecf22b52c20703fae5f3bc30c06
James Cook <jcook.rubyist@gmail.com>
2025-10-25 08:59:17 -0500

    Simplecov

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "css_parser", "~> 1.0" # for benchmarking against
 gem "webmock", "~> 3.0" # for testing URL loading
 gem 'minitest'
 gem 'minitest-spec'
+gem 'simplecov', require: false
---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+# Load SimpleCov for code coverage when COVERAGE env var is set
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/test/'
+    add_filter '/benchmarks/'
+
+    # Group coverage by component
+    add_group 'Parser', 'lib/cataract/parser.rb'
+    add_group 'RuleSet', 'lib/cataract/rule_set.rb'
+    add_group 'Declarations', 'lib/cataract/declarations.rb'
+  end
+end
+
+require 'minitest/autorun'
+require 'cataract'
